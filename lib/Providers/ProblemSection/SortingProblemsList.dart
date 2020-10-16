7a520b3f1088ffb387e2885d0891a20dde9eebc3
Shruti-1910 <skshrutikumari.46@gmail.com>
2020-10-16 14:52:35 +0530

    Added problems to sorting

--- lib/Providers/ProblemSection/SortingProblemsList.dart
+++ lib/Providers/ProblemSection/SortingProblemsList.dart
@@ -2,8 +2,59 @@ import 'Single_Problem.dart';
 import 'package:flutter/material.dart';
 
 class SortingProblems extends ChangeNotifier {
-  List<SingleProblem> SortingProb = [];
-
+  List<SingleProblem> SortingProb = [
+    SingleProblem(
+      title: "Choose Two Numbers",
+      difficulty: "Easy",
+      input: "1\n20\n2\n10 20\n\n3\n3 2 2\n5\n1 5 7 7 9",
+      output: "10 20\n\n3 1",
+      description:
+          "Given an array A, consisting of n positive integers and an array B, consisting of m positive integers. You have to choose 'a' elements from array A and 'b' elements from array B such that a+b does not belong to either array A or array B. If there are multiple answers, choose and print any set of two numbers.",
+      inputExplain:
+          "In the first example, we can choose 20 from array [20] and 20 from array [10,20]. Number 40=20+20 doesnt belong to any of those arrays. However, it is possible to choose 10 from the second array too.\n\nIn the second example, we can choose 3 from array [3,2,2] and 1 from array [1,5,7,7,9]. Number 4=3+1 doesnt belong to any of those arrays\n\n",
+      constraints: "1=n=100\na=a=200\n1=m=100\n1=b=200",
+      tags: "null",
+      prereq: "null",
+      explaination: "null",
+      code: "null",
+      inputFormat:
+          "The first line contains one integer n- the number of elements of array A.\nThe second line contains n integers a1, a2,...,an- the number of elements of A.\nThe third line contains one integer m- the number of elements of B.\nThe fourth line contains m different integers b1,b2,...,bm- the elements of array of B",
+    ),
+    SingleProblem(
+      title: "Twins",
+      difficulty: "Medium",
+      input: "2\n3 3\n\n3\n2 1 2",
+      output: "2\n\n2",
+      description:
+          "Imagine that you have a Twin brother or sister. Now let's imagine a typical morning in your family, your Mom is already late for work and in haste fished for some coins, that are exactly n numbers, for you both to buy lunch from the school cafeteria. She scribbled a note in which she wrote to split the coins equally, but as you found the note befor your twin you decided to pick for yourself some subset of coins so that the sum of values of your coins is 'strictly larger' than the sum values of the remaining coins. To avoid suspicion you take the minimum number of coins, whose sum of values is strictly more than the sum of values of the remaining coins. Determine the minimum number of coins you need to take to divide them in the described manner.",
+      inputExplain:
+          "In the first sample you will have to take 2 coins (you and your twin have sums equal to 6,0 correspondingly). If you take 1 coin, you get sums 3,3. If you take 0 coins, you get sums 0,6. Those variants do not satisfy you as your sum should be strictly more that your twins' sum.\n\nIn the second sample one coin isn't enough for us, too. You can pick coins with values 1,2 or 2,2. In any case, the minimum number of coins equals 2.\n\n",
+      constraints: "1=n=100\n1=a=100, where a is value the coin",
+      tags: "null",
+      prereq: "null",
+      explaination: "null",
+      code: "null",
+      inputFormat:
+          "The first line cointains integer n- the number of coins.\nThe second line contains a1, a2,...an-the value of the coins.",
+    ),
+    SingleProblem(
+      title: "Sum Query",
+      difficulty: "Hard",
+      input: "5\n5 8 3 15 12\n2\n5 12\n4 8",
+      output: "18\n30",
+      description:
+          "Given an array A which contains N elements, you are also provided two numbers X and Y (it may or may not be present in the array). You have to perform these tasks:\n 1.Sort the array.\n 2.Print the sum of all the numbers that are less than X and greater than Y (X and Y are excluded).\n 3.Perform the second step Q times, here Q is the number of queries.",
+      inputExplain:
+          "In the given input, '5, 8, 3, 15, 12' are the elements of array. According to the question, first step is to sort the array. After sorting we will get the array as: '3, 5, 8, 12, 15'.\n\nFor the 1st query: X=5 and Y=12\nThe numbers smaller than X is '3' and numbers greater than Y is 15. So, for this input the required output will be: 3+15=18.\n\nFor the 2nd query: X=4 and Y=8\nThe numbers less than X are '3' and the numbers greater than Y are '12' and '15'. So, for this query we print the output as: 3+12+15=30.\n\n",
+      constraints: "1=N=10^5\n1=Ai=10^5\n1=Q=10^5\n1=X=Y=10^5",
+      tags: "null",
+      prereq: "null",
+      explaination: "null",
+      code: "null",
+      inputFormat:
+          "The first line contains  an integer N denoting the size of array.\nThe second line contains N integers A1,A2,A3,...,An.\nThe third line contains an integer Q denoting the number of queries.\nNext, Q lines contain two integres X and Y.",
+    ),
+  ];
   List<SingleProblem> get ProblemList {
     return [...SortingProb];
   }
