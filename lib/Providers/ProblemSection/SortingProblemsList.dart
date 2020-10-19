b0e5bd14b0de0e2665a8fbe922325e0dd728c392
Your Name <you@example.com>
2020-10-19 10:14:37 +0530

    .

--- lib/Providers/ProblemSection/SortingProblemsList.dart
+++ lib/Providers/ProblemSection/SortingProblemsList.dart
@@ -12,7 +12,7 @@ class SortingProblems extends ChangeNotifier {
           "Given an array A, consisting of n positive integers and an array B, consisting of m positive integers. You have to choose 'a' elements from array A and 'b' elements from array B such that a+b does not belong to either array A or array B. If there are multiple answers, choose and print any set of two numbers.",
       inputExplain:
           "In the first example, we can choose 20 from array [20] and 20 from array [10,20]. Number 40=20+20 doesnt belong to any of those arrays. However, it is possible to choose 10 from the second array too.\n\nIn the second example, we can choose 3 from array [3,2,2] and 1 from array [1,5,7,7,9]. Number 4=3+1 doesnt belong to any of those arrays\n\n",
-      constraints: "1=n=100\na=a=200\n1=m=100\n1=b=200",
+      constraints: "1 <= n <= 100\n1 <= a <= 200\n1 <= m <= 100\n1 <= b <= 200",
       tags: "null",
       prereq: "null",
       explaination: "null",
@@ -29,7 +29,7 @@ class SortingProblems extends ChangeNotifier {
           "All elements on the right of 7(at index 0)are smaller than or equal to 7.Also, all the elements of right side od 7(at index 3) are smaller than 7.And the last element 3 is itself a leader since no element are on its right.",
       inputExplain:
           "In the first example, we can choose 20 from array [20] and 20 from array [10,20]. Number 40=20+20 doesnt belong to any of those arrays. However, it is possible to choose 10 from the second array too.\n\nIn the second example, we can choose 3 from array [3,2,2] and 1 from array [1,5,7,7,9]. Number 4=3+1 doesnt belong to any of those arrays\n\n",
-      constraints: "1<=N<=10^7\n0<=Ai<=10^7",
+      constraints: "1 <= N <= 10^7\n0 <= Ai <= 10^7",
       tags: "null",
       prereq: "null",
       explaination: "null",
@@ -44,9 +44,8 @@ class SortingProblems extends ChangeNotifier {
       output: "10 20\n\n3 1",
       description:
           "Given an array a consisting of n positive integers.In one move,you can choose two indices i and j such that the absolute difference between ai and aj is no more than one and remove the smallest of these two elements.If two elements are equal, you can remove any ofthem(but exactlyone)\nYour job is to find if it is possible to obtain the array consisting of only one element using several such moves or not. ",
-      inputExplain:
-          "null",
-      constraints: "1<=n<=50\n1<=ai<=100",
+      inputExplain: "null",
+      constraints: "1 <= n <= 50\n1 <= ai <= 100",
       tags: "null",
       prereq: "null",
       explaination: "null",
@@ -72,37 +71,39 @@ class SortingProblems extends ChangeNotifier {
           "The first line cointains integer n- the number of coins.\nThe second line contains a1, a2,...an-the value of the coins.",
     ),
     SingleProblem(
-      title:"Mark and Toys",
+      title: "Mark and Toys",
       difficulty: "Medium",
-      input:"7 50 /n 1 12 5 111 200 1000 10",
+      input: "7 50 \n1 12 5 111 200 1000 10",
       output: "4",
       description:
-         "Imagine Mark and Jane have a son who loves toys, so Mark wants to bug some. There are a number of different toys lying in front of him, tagged with their prices.Mark has only a certain amount to spend, and he wants maximize the number of toys he bugs with this money.  Givena list of prices and amount to spend, for example, if \n Prices = [1,2,3,4] and Mark has k=7 to spend, he can bug items[1,2,3] for 6, or[3,4] for 7 unit of currency. He would choose the first group of 3 items.",     
+          "Imagine Mark and Jane have a son who loves toys, so Mark wants to bug some. There are a number of different toys lying in front of him, tagged with their prices.Mark has only a certain amount to spend, and he wants maximize the number of toys he bugs with this money.  Givena list of prices and amount to spend, for example, if \n Prices = [1,2,3,4] and Mark has k=7 to spend, he can bug items[1,2,3] for 6, or[3,4] for 7 unit of currency. He would choose the first group of 3 items.",
       inputExplain:
-         "In the given sample list you will take price as Prices = [1,2,3,4] and amount as k=7 which Mark has to spend , he can bug items[1,2,3] for 6, or[3,4] for 7 unit of currency. But he  choose the first group of 3 items because it has maximize amount of toys.",   
-      constraints: "1≤n≤10^5/n 1≤k≤10^9/n 1≤prices[i]≤10^9",
-      tags:"null",
+          "In the given sample list you will take price as Prices = [1,2,3,4] and amount as k=7 which Mark has to spend , he can bug items[1,2,3] for 6, or[3,4] for 7 unit of currency. But he  choose the first group of 3 items because it has maximize amount of toys.",
+      constraints: "1 ≤ n ≤ 10^5\n1 ≤ k ≤ 10^9\n1 ≤ prices[i] ≤ 10^9",
+      tags: "null",
       prereq: "null",
       explaination: "null",
       code: "null",
-      inputFormat: "The first line containts two integers: n- the number of priced toys and k- the amount Mark has to spend./n The second line contains n space-separated integers prices[i].",
+      inputFormat:
+          "The first line containts two integers: n- the number of priced toys and k- the amount Mark has to spend./n The second line contains n space-separated integers prices[i].",
     ),
     SingleProblem(
       title: "Closest Number",
       difficulty: "Medium",
-      input: "4/n 5,4,3,2",
-      output:"2,3,3,4,4,5",
-      description: 
-         "Sorting is useful as the first step in many different tasks. The most common task is to make finding things easier, but there are other uses as well. In this case, it will make it easier to determine which pair or pairs of elements have the smallest absolute difference between them.You have to find the pair of elements that have the smallest absolute diffrence beteen them. If there are multiple pairs, find them all.",
-      inputExplain:  
-         "In the given input list[5,2,3,4,1] are the element of array which you have to sort it as[1,2,3,4,5] to see the several pairs have minimum diffrence of 1:[(1,2),(2,3),(3,4)(4,5)]. So the value return array would be[1,2,2,3,3,4,4,5].",
-      constraints: "2≤n≤200000/n -10^7≤arr[i]≤10^7/n All a[i] are unique in arr.",
-      tags:"null",
+      input: "4\n5,4,3,2",
+      output: "2,3,3,4,4,5",
+      description:
+          "Sorting is useful as the first step in many different tasks. The most common task is to make finding things easier, but there are other uses as well. In this case, it will make it easier to determine which pair or pairs of elements have the smallest absolute difference between them.You have to find the pair of elements that have the smallest absolute diffrence beteen them. If there are multiple pairs, find them all.",
+      inputExplain:
+          "In the given input list[5,2,3,4,1] are the element of array which you have to sort it as[1,2,3,4,5] to see the several pairs have minimum diffrence of 1:[(1,2),(2,3),(3,4)(4,5)]. So the value return array would be[1,2,2,3,3,4,4,5].",
+      constraints:
+          "2 ≤ n ≤ 200000\n-10^7 ≤ arr[i] ≤ 10^7\nAll a[i] are unique in arr.",
+      tags: "null",
       prereq: "null",
       explaination: "null",
-      code:"null",
-      inputFormat: "The first line contains a single integer n, the lenght of arr./n The second line contains n space-seprated integer,arr[i].",          
-
+      code: "null",
+      inputFormat:
+          "The first line contains a single integer n, the lenght of arr./n The second line contains n space-seprated integer,arr[i].",
     ),
     SingleProblem(
       title: "Sum Query",
