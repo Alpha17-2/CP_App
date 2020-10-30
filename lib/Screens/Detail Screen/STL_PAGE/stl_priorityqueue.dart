8a90374ed93cd026b0ec270ebf702892782e1679
swaadheenta <gayatriagnani7@gmail.com>
2020-10-30 19:34:07 +0530

    queue and priority queue added

---
+++ lib/Screens/Detail Screen/STL_PAGE/stl_priorityqueue.dart
@@ -0,0 +1,464 @@
+import 'package:flutter/material.dart';
+import 'package:CP_App/Helpers/DeviceSize.dart';
+
+class Stl_Priorityqueue extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double smallDivider = displayHeight(context) * 0.028;
+    double verysmallDivider = displayHeight(context) * 0.01;
+    double fontsize = displayWidth(context) * 0.045;
+    double headingfontsize = displayWidth(context) * 0.062;
+    double functionfontsize = displayWidth(context) * 0.055;
+    final text1 = new RichText(
+      text: TextSpan(
+          style: TextStyle(
+              fontSize: fontsize,
+              color: Colors.black,
+              fontWeight: FontWeight.bold),
+          children: <TextSpan>[
+            new TextSpan(text: "In this data structure every element has an priority associated with it and the elements are inserted in this container on the basis of their priority . The element with "),
+            new TextSpan(
+                text: "greatest ",
+                style:
+                    TextStyle(color: Colors.red, fontWeight: FontWeight.bold)),
+            new TextSpan(text: "value has the "),
+            new TextSpan(
+                text: "highest ",
+                style:
+                    TextStyle(color: Colors.red, fontWeight: FontWeight.bold)),
+            new TextSpan(
+                text:
+                    " priority .So we can say that elements are arranged in "),
+            new TextSpan(
+                text:"decreasing",
+                 style:
+                    TextStyle(color: Colors.red, fontWeight: FontWeight.bold)
+                ),
+            new TextSpan(text:"order in this type of queue.\n\n"),
+            new TextSpan(
+              text: "To use priority queue :- \n",
+              style: TextStyle(
+                  color: Colors.black54,
+                  fontWeight: FontWeight.bold,
+                  fontSize: displayWidth(context) * 0.048),
+            ),
+            new TextSpan(
+                text: "#include<queue> ",
+                style: TextStyle(
+                    letterSpacing: 0.8,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                    fontSize: displayWidth(context) * 0.042)),
+            new TextSpan(
+                text: "or ",
+                style: TextStyle(fontSize: displayWidth(context) * 0.042)),
+            new TextSpan(
+                text: "#include<bits/stdc++.h>\n\n",
+                style: TextStyle(
+                    letterSpacing: 0.8,
+                    fontSize: displayWidth(context) * 0.042,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold)),
+            new TextSpan(
+              text: "Syntax : \n",
+              style: TextStyle(
+                  color: Colors.black54,
+                  fontWeight: FontWeight.bold,
+                  fontSize: displayWidth(context) * 0.048),
+            ),
+            new TextSpan(
+              text: "priority_queue<data_type>stack_name;\n\n",
+              style: TextStyle(
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold,
+                  fontSize: displayWidth(context) * 0.042),
+            ),
+            new TextSpan(
+              text: "Example : ",
+              style: TextStyle(
+                  color: Colors.black54,
+                  fontWeight: FontWeight.bold,
+                  fontSize: displayWidth(context) * 0.048),
+            ),
+          ]),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Priority queue"),
+        backgroundColor: Colors.blue[200],
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding:
+            EdgeInsets.only(top: 16.0, bottom: 12.0, left: 8.0, right: 8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              text1,
+              Opacity(
+                  opacity: 0.0,
+                  child: Divider(
+                    height: verysmallDivider,
+                  )),
+              Text(
+                "priority_queue<int>q ;",
+                style: TextStyle(
+                    letterSpacing: 0.8,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                    fontSize: displayWidth(context) * 0.042),
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Divider(
+                  height: smallDivider,
+                ),
+              ),
+              Text(
+                "Functions of priority queue :-",
+                style: TextStyle(
+                    color: Colors.blue[700],
+                    fontWeight: FontWeight.bold,
+                    fontSize: headingfontsize),
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Divider(
+                  height: smallDivider,
+                ),
+              ),
+              Text(
+                "1. push(value):",
+                style: TextStyle(
+                    fontSize: functionfontsize,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold),
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Divider(
+                  height: displayHeight(context) * 0.008,
+                ),
+              ),
+              Text(
+                "It is used to insert elements in a queue which are then arranged automatically in non increasing order according to their priority.",
+                style: TextStyle(fontSize: fontsize),
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Divider(
+                  height: displayHeight(context) * 0.01,
+                ),
+              ),
+              Text(
+                "Syntax : ",
+                style: TextStyle(
+                    fontSize: fontsize,
+                    color: Colors.orange[800],
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "priorityqueue_name.push(val);",
+                style:
+                    TextStyle(fontSize: fontsize, fontWeight: FontWeight.bold),
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Divider(
+                  height: displayHeight(context) * 0.01,
+                ),
+              ),
+              Text("Example: ",
+                  style: TextStyle(
+                    fontSize: fontsize,
+                    color: Colors.orange[800],
+                    fontWeight: FontWeight.bold,
+                  )),
+              Text(
+                "q.push(value) :",
+                style: TextStyle(
+                    letterSpacing: 0.8,
+                    fontSize: fontsize,
+                    fontWeight: FontWeight.bold),
+              ),
+              Divider(
+                height: smallDivider,
+                color: Colors.black,
+                thickness: 1.0,
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Divider(
+                  height: verysmallDivider,
+                ),
+              ),
+              Text(
+                "2. pop() :",
+                style: TextStyle(
+                    fontSize: functionfontsize,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold),
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Divider(
+                  height: displayHeight(context) * 0.008,
+                ),
+              ),
+              Text(
+                "It is used to pop or remove the element present at the top of the queue",
+                style: TextStyle(fontSize: fontsize),
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Divider(
+                  height: displayHeight(context) * 0.01,
+                ),
+              ),
+              Text(
+                "Syntax : ",
+                style: TextStyle(
+                    fontSize: fontsize,
+                    color: Colors.orange[800],
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "priorityqueue_name.pop();",
+                style:
+                    TextStyle(fontSize: fontsize, fontWeight: FontWeight.bold),
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Divider(
+                  height: displayHeight(context) * 0.01,
+                ),
+              ),
+              Text("Example: ",
+                  style: TextStyle(
+                    fontSize: fontsize,
+                    color: Colors.orange[800],
+                    fontWeight: FontWeight.bold,
+                  )),
+              Text(
+                "q.pop();",
+                style: TextStyle(
+                    letterSpacing: 0.8,
+                    fontSize: fontsize,
+                    fontWeight: FontWeight.bold),
+              ),
+              Divider(
+                height: smallDivider,
+                color: Colors.black,
+                thickness: 1.0,
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Divider(
+                  height: verysmallDivider,
+                ),
+              ),
+              Text(
+                "3. top() :",
+                style: TextStyle(
+                    fontSize: functionfontsize,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold),
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Divider(
+                  height: displayHeight(context) * 0.008,
+                ),
+              ),
+              Text(
+                "It is used to access the element which is present at the top of the priority queue.",
+                style: TextStyle(fontSize: fontsize),
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Divider(
+                  height: displayHeight(context) * 0.01,
+                ),
+              ),
+              Text(
+                "Syntax : ",
+                style: TextStyle(
+                    fontSize: fontsize,
+                    color: Colors.orange[800],
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "priorityqueue_name.top()",
+                style:
+                    TextStyle(fontSize: fontsize, fontWeight: FontWeight.bold),
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Divider(
+                  height: displayHeight(context) * 0.01,
+                ),
+              ),
+              Text("Example: ",
+                  style: TextStyle(
+                    fontSize: fontsize,
+                    color: Colors.orange[800],
+                    fontWeight: FontWeight.bold,
+                  )),
+              Text(
+                "int val= q.top();",
+                style: TextStyle(
+                    letterSpacing: 0.8,
+                    fontSize: fontsize,
+                    fontWeight: FontWeight.bold),
+              ),
+              Divider(
+                height: smallDivider,
+                color: Colors.black,
+                thickness: 1.0,
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Divider(
+                  height: verysmallDivider,
+                ),
+              ),
+              Text(
+                "4. size() :",
+                style: TextStyle(
+                    fontSize: functionfontsize,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold),
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Divider(
+                  height: displayHeight(context) * 0.008,
+                ),
+              ),
+              Text(
+                "It returns the size of the priority queue.",
+                style: TextStyle(fontSize: fontsize),
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Divider(
+                  height: displayHeight(context) * 0.01,
+                ),
+              ),
+              Text(
+                "Syntax : ",
+                style: TextStyle(
+                    fontSize: fontsize,
+                    color: Colors.orange[800],
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "priorityqueue_name.size();",
+                style:
+                    TextStyle(fontSize: fontsize, fontWeight: FontWeight.bold),
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Divider(
+                  height: displayHeight(context) * 0.01,
+                ),
+              ),
+              Text("Example: ",
+                  style: TextStyle(
+                    fontSize: fontsize,
+                    color: Colors.orange[800],
+                    fontWeight: FontWeight.bold,
+                  )),
+              Text(
+                "int N= q.size();",
+                style: TextStyle(
+                    letterSpacing: 0.8,
+                    fontSize: fontsize,
+                    fontWeight: FontWeight.bold),
+              ),
+              Divider(
+                height: smallDivider,
+                color: Colors.black,
+                thickness: 1.0,
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Divider(
+                  height: verysmallDivider,
+                ),
+              ),
+              Text(
+                "5. empty() :",
+                style: TextStyle(
+                    fontSize: functionfontsize,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold),
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Divider(
+                  height: displayHeight(context) * 0.008,
+                ),
+              ),
+              Text(
+                "It returns true when the priority queue is empty else it returns false.",
+                style: TextStyle(fontSize: fontsize),
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Divider(
+                  height: displayHeight(context) * 0.01,
+                ),
+              ),
+              Text(
+                "Syntax : ",
+                style: TextStyle(
+                    fontSize: fontsize,
+                    color: Colors.orange[800],
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "priorityqueue_name.empty();",
+                style:
+                    TextStyle(fontSize: fontsize, fontWeight: FontWeight.bold),
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Divider(
+                  height: displayHeight(context) * 0.01,
+                ),
+              ),
+              Text("Example: ",
+                  style: TextStyle(
+                    fontSize: fontsize,
+                    color: Colors.orange[800],
+                    fontWeight: FontWeight.bold,
+                  )),
+              Text(
+                "bool isempty = q.empty();",
+                style: TextStyle(
+                    letterSpacing: 0.8,
+                    fontSize: fontsize,
+                    fontWeight: FontWeight.bold),
+              ),
+              Divider(
+                height: smallDivider,
+                color: Colors.black,
+                thickness: 1.0,
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Divider(
+                  height: verysmallDivider,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
