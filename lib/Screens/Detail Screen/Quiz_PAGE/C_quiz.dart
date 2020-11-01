c055ea60257789a19fcc7ba086b90269b745181f
Your Name <you@example.com>
2020-11-01 20:11:54 +0530

    20% work on C quiz page done

--- lib/Screens/Detail Screen/Quiz_PAGE/C_quiz.dart
+++ lib/Screens/Detail Screen/Quiz_PAGE/C_quiz.dart
@@ -21,45 +21,48 @@ class _C_QuizState extends State<C_Quiz> {
     return Hero(
       tag: "Cquiz",
       child: Scaffold(
-        appBar: AppBar(
-          elevation: 0.0,
-          backgroundColor: const Color(0xffea5d49),
-        ),
-        body: Stack(
-          alignment: Alignment.center,
-          children: [
-            CustomPaint(
+          appBar: AppBar(
+            elevation: 0.0,
+            backgroundColor: const Color(0xffea5d49),
+          ),
+          body: Container(
+            child: CustomPaint(
               child: Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Opacity(
-                      opacity: 0.0,
-                      child: Divider(
-                        height: displayHeight(context) * 0.02,
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 50.0, left: 8.0, right: 8.0),
+                        child: Center(
+                          child: Text(
+                            problem,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: displayWidth(context) * 0.06,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
                       ),
-                    ),
-                    Center(
-                      child: Text(
-                        problem,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: displayWidth(context) * 0.05,
-                            fontWeight: FontWeight.bold),
+                      Opacity(
+                        opacity: 0.0,
+                        child: Divider(
+                          height: displayHeight(context) * 0.35,
+                        ),
                       ),
-                    ),
-                  ],
+                      Text("hwlllo")
+                    ],
+                  ),
                 ),
                 width: MediaQuery.of(context).size.width,
                 height: MediaQuery.of(context).size.height,
               ),
               painter: HeaderCurvedContainer(),
             ),
-          ],
-        ),
-      ),
+          )),
     );
   }
 }
